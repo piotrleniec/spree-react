be3b5881deb594b358fba44b1ea8b3923406a5d6
Piotr Leniec <leniec.piotr@gmail.com>
2017-04-09 20:17:55 +0200

    Redux DevTools setup.

--- client/app/components/App.js
+++ client/app/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+import DevTools from './DevTools'
 import store from '../store'
 import HomePage from './HomePage'
 
 export default () => (
   <Provider store={store}>
-    <HomePage />
+    <div>
+      <HomePage />
+      <DevTools />
+    </div>
   </Provider>
 )
