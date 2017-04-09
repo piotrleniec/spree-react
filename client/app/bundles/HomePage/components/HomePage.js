1edfda84b09f0b5797a8130439557937001e5053
Piotr Leniec <leniec.piotr@gmail.com>
2017-04-09 16:58:49 +0200

    Products fetching.

--- client/app/bundles/HomePage/components/HomePage.js
+++ client/app/bundles/HomePage/components/HomePage.js
@@ -1,5 +1,29 @@
 import React from 'react'
 
-export default () => (
-  <h1>Hello from Home Page!</h1>
-)
+export default class extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { products: [], currentPage: 0, pages: 0 }
+  }
+
+  componentDidMount() {
+    fetch('/api/v1/products?page=1&per_page=12')
+      .then(response => response.json())
+      .then(({ products, current_page: currentPage, pages }) => {
+        this.setState({ products, currentPage, pages })
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.products.map(product => (
+          <div>
+            {JSON.stringify(product)}
+          </div>
+        ))}
+      </div>
+    )
+  }
+}
