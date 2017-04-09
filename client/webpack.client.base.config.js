234e4f603ad58c89c35a742ac0ec51c5e418bfae
Piotr Leniec <leniec.piotr@gmail.com>
2017-04-09 20:00:31 +0200

    Client directory structure rework.

--- client/webpack.client.base.config.js
+++ client/webpack.client.base.config.js
@@ -16,7 +16,7 @@ module.exports = {
     // This will contain the app entry points defined by
     // webpack.client.rails.hot.config and webpack.client.rails.build.config
     app: [
-      './app/bundles/HomePage'
+      './app'
     ],
   },
   resolve: {
