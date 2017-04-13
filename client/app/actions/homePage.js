98bb988c74d805edaecfc51f2f04a815461be268
Piotr Leniec <leniec.piotr@gmail.com>
2017-04-13 19:16:24 +0200

    Get current page from url.

--- client/app/actions/homePage.js
+++ client/app/actions/homePage.js
@@ -1,16 +1,15 @@
 import { SET_PRODUCTS } from '../actionTypes'
 
-const setProducts = (products, currentPage, pages) => ({
+const setProducts = (products, pages) => ({
   type: SET_PRODUCTS,
   products,
-  currentPage,
   pages
 })
 
 export const fetchProducts = page => dispatch => {
   fetch(`/api/v1/products?page=${page}&per_page=12`)
     .then(response => response.json())
-    .then(({ products, current_page: currentPage, pages }) => {
-      dispatch(setProducts(products, currentPage, pages))
+    .then(({ products, pages }) => {
+      dispatch(setProducts(products, pages))
     })
 }
--- client/app/reducers/homePage.js
+++ client/app/reducers/homePage.js
@@ -2,18 +2,13 @@ import { SET_PRODUCTS } from '../actionTypes'
 
 const initialState = {
   products: [],
-  currentPage: 1,
   pages: 0
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case SET_PRODUCTS:
-      return {
-        products: action.products,
-        currentPage: action.currentPage,
-        pages: action.pages
-      }
+      return { products: action.products, pages: action.pages }
     default:
       return state
   }
